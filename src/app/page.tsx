da3dc6f4df10ddc6ccbb5f307647b21a1f858c60
divyanshjainsab <divyansh200325@gmail.com>
2026-02-06 17:44:44 +0530

    Initial Next.js app with Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-green-600">
+      Dev mode is running 🚀
+    </h1>
+  );
+}
